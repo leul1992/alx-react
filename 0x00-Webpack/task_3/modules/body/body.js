8d61b248e0a3198fa5f11649feea40d032875060
Leulseged <leulbekele191@gmail.com>
2023-01-27 23:08:56 +0300

    set up dev server

---
+++ 0x00-Webpack/task_3/modules/body/body.js
@@ -0,0 +1,15 @@
+import './body.css';
+import $  from 'jquery';
+import _ from 'lodash';
+
+let counter = 0;
+
+$("<button>Click here to get started</button>").appendTo('body');
+$("<p id='count'></p>").appendTo('body');
+
+function updateCounter() {
+  counter++;
+  $('#count').html(`${counter} clicks on the button`);
+}
+
+$("button").on("click", _.debounce(updateCounter, 500));
