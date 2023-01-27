f08eb59551bbb520c1ae0d7a945e400d935e5f4b
Leulseged <leulbekele191@gmail.com>
2023-01-27 19:00:20 +0300

    config webpack

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,12 +1,12 @@
 const path = require("path");
 
 module.exports = {
-    mode: 'production',
-    entry: {
-        main: path.resolve(__dirname, './js/dashboard_main.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js',
-    },
+  mode: 'production',
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
 };
