ffe5979bcce29ba17e47c45311dd90265cab3873
Leulseged <leulbekele191@gmail.com>
2023-02-03 23:53:03 +0300

    deploy

---
+++ 0x01-react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import logo from './HolbertonLogo.jpg';
+import './App.css';
+import {getFullYear, getFooterCopy} from './utils'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+        <hr className="hr"></hr>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email : </label>
+        <input name="email" type="email" id="email"></input>
+        <label htmlFor="password">Password : </label>
+        <input name="password" type="password" id="password"></input>
+        <button>OK</button>
+      </body>
+      <footer className="App-footer">
+        <hr className="hr"></hr>
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ 0x01-react_intro/task_4/dashboard/src/utils.js
@@ -0,0 +1,21 @@
+function getFullYear(){
+    return(
+        new Date().getFullYear()
+        );
+}
+
+function getFooterCopy(isIndex) {
+    return (
+        isIndex ? "Holberton School" : "Holberton School main dashboard"
+        );
+}
+
+function getLatestNotification() {
+    return ("<strong>Urgent requirement</strong> - complete by EOD")
+}
+
+export {
+    getFullYear,
+    getFooterCopy,
+    getLatestNotification,
+}
