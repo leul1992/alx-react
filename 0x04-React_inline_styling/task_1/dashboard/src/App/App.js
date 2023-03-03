f27129d38423d83aaed775122db7b14ac68a3bb5
Leulseged <leulbekele191@gmail.com>
2023-03-03 22:48:15 +0300

    inline style

--- 0x04-React_inline_styling/task_1/dashboard/src/App/App.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/App/App.js
@@ -51,7 +51,7 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <Notification listNotifications={this.listNotifications}/>
-        <div className={css(body.App)}>
+        <div className={css(bodyStyles.App)}>
           <Header />
           {this.props.isLoggedIn ?
             <BodySectionWithMarginBottom title="Course list"><CourseList listCourses={this.listCourses}/></BodySectionWithMarginBottom>
@@ -70,7 +70,7 @@ class App extends React.Component {
   }
 }
 
-const body = StyleSheet.create({
+const bodyStyles = StyleSheet.create({
   App: {
     position: 'relative',
     minHeight: '100vh'
