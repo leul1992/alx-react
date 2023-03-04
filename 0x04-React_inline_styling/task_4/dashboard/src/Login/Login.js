5f5cbb015777164b1f73388e52de5cb04198eea7
Leulseged <leulbekele191@gmail.com>
2023-03-04 12:52:43 +0300

    add animation

---
+++ 0x04-React_inline_styling/task_4/dashboard/src/Login/Login.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+function Login() {
+  return (
+    <React.Fragment>
+      <div className={css(loginStyles.appBody)}>
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email" name="email" className={loginStyles.inputs} />
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" name="password" className={loginStyles.inputs} />
+        <button className={css(loginStyles.button)}>OK</button>
+      </div>
+    </React.Fragment>
+  )
+}
+
+const loginStyles = StyleSheet.create({
+	appBody: {
+    padding: '36px 24px',
+		'@media (max-width: 900px)': {
+      display: 'flex',
+      flexDirection: 'column',
+      width: "40%",
+    }
+	},
+
+	inputs: {
+		margin: '0 16px 0 8px',
+    '@media (max-width: 900px)': {
+      border: '3px solid transparent',
+      width: '40%',
+    }
+	},
+  button: {
+    '@media (max-width: 900px)': {
+      border: '3px solid #d4b383',
+      width: '45px',
+      borderRadius: '6%',
+      padding: '4px',
+      ':hover': {
+        cursor: 'pointer',
+      }
+    }
+  }
+})
+
+
+export default Login;
