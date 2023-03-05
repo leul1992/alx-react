6b5601c96a8528c289b655a0f2858e9ff2a670c0
Leulseged <leulbekele191@gmail.com>
2023-03-05 20:19:18 +0300

    nested element

---
+++ 0x06-React_Immutable/2-nested.js
@@ -0,0 +1,5 @@
+import { fromJS } from './node_modules/immutable/dist/immutable';
+
+export default function accessImmutableObject(object, array) {
+  return fromJS(object).getIn(array, undefined);
+}
--- 0x06-React_Immutable/test.js
+++ 0x06-React_Immutable/test.js
@@ -1,9 +1,7 @@
-import getImmutableObject from "./1-map";
-console.log(getImmutableObject(
-    {
-        fear: true,
-        smell: -1033575916.9145899,
-        wall: false,
-        thing: -914767132
-   }
-))
+import accessImmutableObject from "./2-nested";
+console.log(accessImmutableObject({
+    name: {
+         first: "Guillaume",
+         last: "Salva"
+    }
+}, ['name', 'first']))
