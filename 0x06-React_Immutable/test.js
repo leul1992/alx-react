125ceb6aedbfe55c5e0540696137bc288b94bc37
Leulseged <leulbekele191@gmail.com>
2023-03-05 20:35:21 +0300

    chained mutation

--- 0x06-React_Immutable/test.js
+++
@@ -1,7 +0,0 @@
-import accessImmutableObject from "./2-nested";
-console.log(accessImmutableObject({
-    name: {
-         first: "Guillaume",
-         last: "Salva"
-    }
-}, ['name', 'first']))
