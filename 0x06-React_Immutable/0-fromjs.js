536853768d57a97c1b1fe66296a282fd5cf1809a
Leulseged <leulbekele191@gmail.com>
2023-03-05 19:05:19 +0300

    convert to immutable

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,4 +1,5 @@
-import { FromJS, fromJS } from "./node_modules/immutable/dist/immutable";
+import { fromJS } from './node_modules/immutable/dist/immutable';
+
 export default function getImmutableObject(object) {
   return fromJS(object);
 }
